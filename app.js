9f795684dfcd92761f54e5eb46a44c10786264e9
KhailendraActivantDeveloper <khailendraprasad.deskmoz@gmail.com>
2026-05-26 15:16:12 +0530

    add rate limitter

--- app.js
+++ app.js
@@ -61,14 +61,14 @@ app.use(
 app.use(cookieParser());
 
 // Apply rate limit to all requests
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 50, // limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+// const limiter = rateLimit({
+//   windowMs: 1 * 60 * 1000, // 15 minutes
+//   max: 50, // limit each IP to 100 requests per windowMs
+//   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// });
 
-app.use(limiter); // apply to all requests
+// app.use(limiter); // apply to all requests
 
 app.use(
   express.json({
