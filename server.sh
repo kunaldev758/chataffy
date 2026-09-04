7897044c6659ed8592a69a95938fa43237b7fbb1
harshita <harshitakumawat.deskmoz@gmail.com>
2026-09-04 12:04:04 +0530

    backend

--- server.sh
+++ server.sh
@@ -19,7 +19,7 @@ echo "========================================"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ENV_SRC="${SCRIPT_DIR}/.env.production"
-SSH_KEY="C:/Users/sta/Desktop/chataffy-imp-data/chataffy-live.pem"
+SSH_KEY="C:\chataffy/chataffy-live.pem"
 SSH_HOST="ubuntu@3.231.129.216"
 REMOTE_APP_DIR="/var/www/chataffy.com/chataffy_be"
 ENV_DST="${SSH_HOST}:${REMOTE_APP_DIR}/.env"
